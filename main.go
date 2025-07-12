28451d1ee04dcb8efdd0dd6110e5aded906be11f
caden <cadenjunan@gmail.com>
2025-07-12 22:43:42 +0800

    step one completed and added test file

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+func readBytes(path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("error while reading %s, cause: %s", path, err.Error())
+	}
+	fmt.Printf("%d %s", len(data), path)
+	return nil
+}
+
+func main() {
+	byteCountCmd := flag.Bool("c", false, "add flag -c to count the number of bytes in the file")
+	flag.Parse()
+	if *byteCountCmd {
+		err := readBytes(flag.Arg(0))
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 }
