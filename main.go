236df0ad5ae63b87de4cd61d2ff8fd74ac173cb1
caden <cadenjunan@gmail.com>
2025-07-12 23:08:43 +0800

    finish step 2

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,17 +13,37 @@ func readBytes(path string) error {
 	if err != nil {
 		return fmt.Errorf("error while reading %s, cause: %s", path, err.Error())
 	}
-	fmt.Printf("%d %s", len(data), path)
+	fmt.Printf("%d %s\n", len(data), path)
+	return nil
+}
+func lineCount(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("error while opening %s, cause: %s", path, err.Error())
+	}
+	reader := bufio.NewReader(f)
+	lines := 0
+	cond: for {
+		_, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break cond
+		}
+		lines ++
+	}
+	fmt.Printf("%d %s\n",lines, path)
 	return nil
 }
 
 func main() {
 	byteCountCmd := flag.Bool("c", false, "add flag -c to count the number of bytes in the file")
+	lineCountCmd := flag.Bool("l", false, "add flag -l to count the number of bytes in the file")
 	flag.Parse()
 	if *byteCountCmd {
 		err := readBytes(flag.Arg(0))
 		if err != nil {
 			panic(err.Error())
 		}
+	}else if *lineCountCmd {
+		lineCount(flag.Arg(0))
 	}
 }
